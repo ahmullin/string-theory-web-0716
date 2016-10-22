a6a01b3f527780e891e569ea8b57c5ee6ceef19b
Alison <alisonhmullin@gmail.com>
2016-10-22 11:29:17 -0400

    solution

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,14 @@
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  result = {}
+  characters_to_sort.split("").each do |char|
+    counter = 0
+    long_text.split("").each do |letter|
+      if char == letter
+        counter += 1
+      end
+    end
+    result[char] = counter
+  end
+    result = result.sort_by {|k,v| v}.reverse.to_h
+    result.keys.join("")
 end
